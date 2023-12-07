afab0c06ca732f18df81353cafe69dab9b73e1ae
Rares <rares.amza01@e-uvt.ro>
2023-12-07 21:51:41 +0200

    config done

---
+++ film randomizer/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-xl text-green-700 flex items-center justify-center'>
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
